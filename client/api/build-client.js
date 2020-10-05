cbc8e149720a9f57ad915b912f360d6e0ab96f9e
Giuliano Gabella <ggabella91@gmail.com>
2020-10-04 17:26:54 -0700

    Added an api helper function for the client that determines what url to make axios requests to when getting the initial props on the landing page. Added conditional rendering that lets the user know whether or not they are signed in, on the landing page.

---
+++ client/api/build-client.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export default ({ req }) => {
+  if (typeof window === 'undefined') {
+    // We are on the server
+    // requests should be made to
+    // http://ingress-nginx-controller.ingress-nginx.svc...
+
+    return axios.create({
+      baseURL:
+        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      headers: req.headers,
+    });
+  } else {
+    // We are on the browser!
+    // Requests can be made with a base URL of '/'
+    return axios.create({
+      baseURL: '/',
+    });
+  }
+};
